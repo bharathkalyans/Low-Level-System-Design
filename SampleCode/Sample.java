2a1c4032e36091b90d09ce3cfc8475b61aac5757
bharathkalyans <sbharath35@gmail.com>
2022-10-16 11:10:49 +0530

    Deleted Sample Folder, Done with Git Basics!!

--- SampleCode/Sample.java
+++
@@ -1,9 +0,0 @@
-class Sample {
-    public static void main(String[] args) {
-        sayHello();
-    }
-
-    private static void sayHello() {
-        System.out.println("I am saying Hello!");
-    }
-}
